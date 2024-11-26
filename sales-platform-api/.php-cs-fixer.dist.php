f10a1c04592c454b0809d3013fba2d34b53e2510
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-11-26 20:37:34 +0100

    setup php-cs-fixer

---
+++ sales-platform-api/.php-cs-fixer.dist.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the sxbrsky/sales-platform.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/public',
+    ])
+    ->append([__FILE__])
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true)
+;
+
+$config = new PhpCsFixer\Config();
+$config->setFinder($finder)
+    ->setRules([
+        '@PSR12' => true
+    ])
+;
+
+return $config;
